0cbd13f4de80d23e7eb8a8dd604344908af62fcc
EmmanuelHmar <eman.hmar@gmail.com>
2018-03-25 23:18:48 -0400

    added db from xcode

---
+++ Db/Db/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Db
+//
+//  Created by puipui hmar on 3/25/18.
+///Users/puipui/Desktop/Db/Db/ViewController.swift  Copyright © 2018 hmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Username
+        
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var username: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
